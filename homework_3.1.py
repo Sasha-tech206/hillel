c7a521abeca9a41ec4cff9cdf943b8a9486d4eb0
Sacha-tech206 <mytestmail813@gmail.com>
2024-12-16 23:51:54 +0200

    Homework update_3.1.

--- homework_3.1.py
+++ homework_3.1.py
@@ -1,18 +1,19 @@
-num1 = float(input("Please enter you first number: "))
+num1 = float(input("Please enter your first number: "))
 operation = input("Please Enter the mathematical operation: ")
-num2 = float(input("Pleasw enter you second number: "))
+num2 = float(input("Please enter your second number: "))
+
 if operation == "+":
-	result = num1 + num2
-	print("Result:", result)
+    result = num1 + num2
+    print("Result:", result)
 elif operation == "-":
-	result = num1 - num2
-	print("Result:", result)
+    result = num1 - num2
+    print("Result:", result)
 elif operation == "*":
-	result = num1*num2
-	print("Result", result)
+    result = num1 * num2
+    print("Result:", result)
 elif operation == "/":
-		if num2 !=0:
-					result = num1 / num2
-					print(result)
-		else:
-			print("Erorr. Division by zero!")
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error: Division by zero!")
