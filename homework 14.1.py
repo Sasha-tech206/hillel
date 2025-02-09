c87036d58bfddbedb9b5756135ce5a8fbdd9a076
Sacha-tech206 <mytestmail813@gmail.com>
2025-02-09 21:58:21 +0200

    Added homework 14.1

---
+++ homework 14.1.py
@@ -0,0 +1,63 @@
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.age} years old, {self.gender}"
+
+
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f"{super().__str__()}, Record Book: {self.record_book}"
+
+
+class GroupLimitError(Exception):
+    """Custom exception for exceeding the group limit."""
+    pass
+
+
+class Group:
+    MAX_STUDENTS = 10
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= self.MAX_STUDENTS:
+            raise GroupLimitError("Cannot add more than 10 students to the group.")
+        self.group.add(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number: {self.number}\n{all_students}'
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+
+gr = Group('PD1')
+try:
+    for i in range(11):
+        gr.add_student(Student('Male', 20 + i, f'Name{i}', f'Surname{i}', f'ID{i}'))
+except GroupLimitError as e:
+    print(e)
+
+print(gr)
